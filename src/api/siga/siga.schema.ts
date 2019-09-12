e5271c98617af3749c6d7ba38f651bf8271d0be6
Antonio Carlos <jr_acn@yahoo.com.br>
2019-09-12 19:53:16 -0300

    Atualização da lib

--- src/api/siga/siga.schema.ts
+++ src/api/siga/siga.schema.ts
@@ -1,12 +1,12 @@
 import * as Joi from '@hapi/joi';
 
 export const login = {
-  query: Joi.object({
+  query: {
     login: Joi
       .string()
       .required(),
     password: Joi
       .string()
       .required(),
-  }).required(),
+  },
 };
